d81477b0046f016860cac4a93b8ad7973625b982
AlfianAzad <User@DESKTOP-L9CBIM9>
2025-06-09 16:24:50 +0700

    Menambahkan Stored Procedure btnUpdate Pemilik

--- Ucppabd/Pemilik.cs
+++ Ucppabd/Pemilik.cs
@@ -142,6 +142,7 @@ namespace Ucppabd
             }
         }
 
+
         private void DataGridViewPemilik_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex >= 0)
