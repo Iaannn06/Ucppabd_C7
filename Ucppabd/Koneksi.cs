9f595992a33f826daf1e6e03c77c4c545c23f02a
AlfianAzad <User@DESKTOP-L9CBIM9>
2025-07-10 17:05:28 +0700

    last

--- Ucppabd/Koneksi.cs
+++ Ucppabd/Koneksi.cs
@@ -2,39 +2,61 @@
 using System.Net;
 using System.Net.Sockets;
 
+
+
 namespace Ucppabd
+
 {
     internal class Koneksi
+
     {
         public string connectionString() //untuk membangun dan mengembalikan string koneksi ke database
-        {
+
+        {
             string connectStr = "";
+
             try
+
             {
                 string localIP = GetLocalIPAddress(); //mendeklarasikan ipaddress
-                connectStr = $"Server={localIP};Initial Catalog=ProjecctPABD;" +
-                             $"Integrated Security=True;";
+                connectStr = $"Server={localIP};Initial Catalog=ProjecctPABD;" +$"Integrated Security=True;";
                 return "Data Source=DESKTOP-L9CBIM9\\SQLEXPRESS01;Initial Catalog=ProjecctPABD;Integrated Security=True;";
             }
+
             catch (Exception ex)
+
             {
                 Console.WriteLine(ex.Message);
                 return string.Empty;
             }
+
         }
 
+
+
         public static string GetLocalIPAddress() //untu mengambil IP Address pada PC yang menjalankan aplikasi
-        {
-            //mengambil infromasi tentang local host
-            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+        {
+            //mengambil infromasi tentang local host
+
+            var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList)
+
             {
+
                 if (ip.AddressFamily == AddressFamily.InterNetwork) //Mengambil IPv4
-                {
+
+                {
                     return ip.ToString();
                 }
+
             }
+
             throw new Exception("Tidak ada alamat IP yang ditemukan.");
+
         }
+
     }
+
 }
+
